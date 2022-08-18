54277a07116626630873d400899b108173736dd8
Gabriel Wilkins <wilkigab001@gmail.com>
2022-08-18 14:15:28 -0600

    problems 1-3

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -6,7 +6,8 @@ var arr = [10,20,30,40,50,60]
   Create a variable named 'firstItem' and set it equal to the first value of the 'arr' array.
 */
 
-//Code Here
+// let firstItem = arr[0]
+// console.log(firstItem)
 
 
 ////////// PROBLEM 2 //////////
@@ -14,7 +15,9 @@ var arr = [10,20,30,40,50,60]
   Remove the last item from the 'arr' array and store it in a variable named 'lastItemRemoved'. Console.log the array to verify you no longer have the last item.
 */
 
-//Code Here
+// let lastItemRemoved = arr.pop()
+// console.log(arr)
+// console.log(lastItemRemoved)
 
 ////////// PROBLEM 3 //////////
 
@@ -26,8 +29,9 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
   Loop through the 'family' array and console.log every item.
 */
 
-//Code Here
-
+for(i = 0; i < family.length; i++){
+  console.log(family[i])
+}
 
 
 ////////// PROBLEM 4 //////////
